b7d5031bd68f2f54cf1d65672d1f32e38d762bb4
webshao <webshao@yahoo.com>
2015-09-23 00:45:12 -0700

    Added author name and changed text color

--- project/ui.R
+++ project/ui.R
@@ -10,7 +10,9 @@ shinyUI(
     fluidPage(    
         
         # Give the page a title
-        titlePanel(h3("Overview of Arrests Made in US States in 1973", align='center')),
+        titlePanel(
+            h3("Overview of Arrests Made in US States in 1973", align='center'),
+        ),
         br(),
         # Generate a row with a sidebar
         sidebarLayout(      
@@ -18,11 +20,13 @@ shinyUI(
             sidebarPanel(
                 helpText("Select the type of crime to see the number of arrests made of that crime in each of the 50 US States in 1973. The number of arrests is normalized to per 100,000 residents."),
                 selectInput("crime.type", "Type of Crime:", choices=colnames(usaRedux[,1:3])),
-                hr(),
+                br(),
                 helpText("Select the grouping of the arrest statistics, by state, division, or region."),
                 selectInput("grouping", "Grouping:", choices=c("State","Division","Region")),
+                br(),
+                helpText("Navigate through the tabbed panels titled Table, Plot, and Map to explore different ways of visualizing the data."),
                 hr(),
-                helpText("Navigate through the tabbed panels titled Table, Plot, and Map to explore different ways of visualizing the data.")
+                helpText("by Weber Shao on 9/23/2015")
             ),
             
             # Make the main panel a set of three tabs: Table, Plot, and Map
