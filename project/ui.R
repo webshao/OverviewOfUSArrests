1f504bb9b9dd4cd7d0d6897934c95bb4019d8a19
webshao <webshao@yahoo.com>
2015-09-23 00:46:29 -0700

    Added author name and changed text color

--- project/ui.R
+++ project/ui.R
@@ -7,26 +7,26 @@ usaRedux <- cbind(USArrests[,c(1,2,4)])
 shinyUI(
     
     # Use a fluid Bootstrap layout
-    fluidPage(    
+    fluidPage(
         
         # Give the page a title
         titlePanel(
-            h3("Overview of Arrests Made in US States in 1973", align='center'),
+            h3("Overview of Arrests Made in US States in 1973", align='center')
         ),
         br(),
         # Generate a row with a sidebar
         sidebarLayout(      
 
             sidebarPanel(
-                helpText("Select the type of crime to see the number of arrests made of that crime in each of the 50 US States in 1973. The number of arrests is normalized to per 100,000 residents."),
+                helpText("Select the type of crime to see the number of arrests made of that crime in each of the 50 US States in 1973. The number of arrests is normalized to per 100,000 residents.", style="color: RoyalBlue"),
                 selectInput("crime.type", "Type of Crime:", choices=colnames(usaRedux[,1:3])),
                 br(),
-                helpText("Select the grouping of the arrest statistics, by state, division, or region."),
+                helpText("Select the grouping of the arrest statistics, by state, division, or region.", style="color: RoyalBlue"),
                 selectInput("grouping", "Grouping:", choices=c("State","Division","Region")),
                 br(),
-                helpText("Navigate through the tabbed panels titled Table, Plot, and Map to explore different ways of visualizing the data."),
+                helpText("Navigate through the tabbed panels titled Table, Plot, and Map to explore different ways of visualizing the data.", style="color: RoyalBlue"),
                 hr(),
-                helpText("by Weber Shao on 9/23/2015")
+                helpText("Created by Weber Shao on 9/23/2015", style="color: gray; font-style: italic")
             ),
             
             # Make the main panel a set of three tabs: Table, Plot, and Map
